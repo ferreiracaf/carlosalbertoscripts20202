ba5200c861747591dbb64407d662f83d39523c23
Carlos Ferreira <carlos.ferreira@insightlab.ufc.br>
2021-04-05 20:22:28 -0300

    Adicionando atividade 19

---
+++ atividades/atividade19/userdata.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y mysql-client
+apt-get install -y apache2 php-mysql php-curl libapache2-mod-php php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip
+
+echo "[client]
+user=admin
+password=123scripts456" > /root/.my.cnf
+
+mysql --defaults-file=/root/.my.cnf -u admin -h PRIVADOIP<<EOF
+CREATE DATABASE wordpress;
+CREATE USER 'USUARIO'@'%' IDENTIFIED BY 'SENHA';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'USUARIO'@'%';
+EOF
+
+rm /root/.my.cnf
+
+BD=wordpress
+USER=USUARIO
+PASSWORD=SENHA
+HOST=PRIVADOIP
+
+cat<<EOF > /etc/apache2/sites-available/wordpress.conf
+<Directory /var/www/html/wordpress/>
+    AllowOverride All
+</Directory>
+EOF
+
+a2enmod rewrite
+a2ensite wordpress
+
+systemctl restart apache2
+systemctl reload apache2
+
+curl -O https://wordpress.org/latest.tar.gz
+
+tar xzvf latest.tar.gz
+
+touch wordpress/.htaccess
+
+cp -a wordpress/. /var/www/html/wordpress
+
+cat<<EOF > wp-config.php
+<?php
+define( 'DB_NAME', '$BD' );
+define( 'DB_USER', '$USER' );
+define( 'DB_PASSWORD', '$PASSWORD' );
+define( 'DB_HOST', '$HOST' );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+
+$(curl -s https://api.wordpress.org/secret-key/1.1/salt/)
+
+\$table_prefix = 'wp_';
+
+define( 'WP_DEBUG', false );
+
+if ( ! defined( 'ABSPATH' ) ) {
+        define( 'ABSPATH', __DIR__ . '/' );
+}
+
+require_once ABSPATH . 'wp-settings.php';
+EOF
+
+cp wp-config.php /var/www/html/wordpress/
+
+chown -R www-data:www-data /var/www/html/wordpress
+
+find /var/www/html/wordpress/ -type d -exec chmod 750 {} \;
+
+find /var/www/html/wordpress/ -type f -exec chmod 640 {} \;
+
+systemctl restart apache2
